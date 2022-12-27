d6dab45629f9c59828484aef4606eb7074b18060
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-27 13:23:40 -0800

    Apped Navbar, footer, and metag tags to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import NavBar from '../compnents/navbar/NavBar'
+import Footer from '../compnents/footer/Footer'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Cyber NFT Minting Platform" />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>)
 }
