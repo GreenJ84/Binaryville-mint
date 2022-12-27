ee468132fd6441dc0bf27e0b7c3019b8ad8a0a56
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-27 13:23:17 -0800

    Moved meta tagginf to app page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Cyber NFT Minting Platform" />
+
       </Head>
       <body>
         <Main />
