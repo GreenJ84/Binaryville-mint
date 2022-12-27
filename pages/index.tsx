8435f1a6d4625e416e1aed8bb4f2f4349a80564e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-27 13:23:03 -0800

    Created and rendered all components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import About from '../compnents/about/About';
+import FAQ from '../compnents/faq/FAQ';
+import Roadmap from '../compnents/roadmap/Roadmap';
+import Team from '../compnents/team/Team';
 
 const css = require("../compnents/home.module.css")
 
 const Home = () => {
   return (
     <div className={ css.main }>
-      {/* <About />
+      <About />
       <Roadmap />
       <Team />
       <FAQ />
-      <Animation /> */}
     </div>
   )
 }
